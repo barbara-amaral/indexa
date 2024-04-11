534ae404e95dadd409ed714d83b1ee2832399fb7
barbara <barbara.souza.amaral@gmail.com>
2024-04-10 21:35:24 -0300

    Implementing the form submission and cancel button tot he form.

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../container/container.component';
 import { DividerComponent } from '../divider/divider.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css',
-  imports: [ContainerComponent, DividerComponent],
+  imports: [ContainerComponent, DividerComponent, ReactiveFormsModule],
 })
-export class ContactFormComponent {}
+export class ContactFormComponent {
+  contactForm!: FormGroup;
+
+  constructor() {
+    this.contactForm = new FormGroup({
+      name: new FormControl(''),
+      phone: new FormControl(''),
+      email: new FormControl(''),
+      birthday: new FormControl(''),
+      social: new FormControl(''),
+      observations: new FormControl(''),
+    });
+  }
+
+  saveContact() {
+    console.log(this.contactForm.value);
+  }
+
+  cancel() {
+    console.log('cancelado');
+  }
+}
